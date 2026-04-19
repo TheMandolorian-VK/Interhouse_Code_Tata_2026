1b9cd6c7ce734ce32c6928b086fbb14e99e40065
TheMandolorian-VK <vivaan.446.2029@doonschool.com>
2026-04-19 20:44:19 +0530

    Create reciever_robot_2.cpp

---
+++ reciever_robot_2.cpp
@@ -0,0 +1,15 @@
+#include <esp_now.h>
+#include <Wifi.h>
+
+void onData(const uint_8 *data, const uint8_t *mac, int len){
+  uint8_t cmd = data[0];
+}
+void setup(){
+  Wifi.mode(WIFI_NOW);
+  esp_now_init();
+  esp_now_register_recv_cb(onData);
+}
+
+void loop{
+  
+}
