f9d3cf223883b5c940e096d48410dc5b351424d9
TheMandolorian-VK <vivaan.446.2029@doonschool.com>
2026-04-19 20:45:53 +0530

    Update reciever_robot_2.cpp

--- reciever_robot_2.cpp
+++ reciever_robot_2.cpp
@@ -5,7 +5,7 @@ void onData(const uint_8 *data, const uint8_t *mac, int len){
   uint8_t cmd = data[0];
 }
 void setup(){
-  Wifi.mode(WIFI_NOW);
+  Wifi.mode(WIFI_STA);
   esp_now_init();
   esp_now_register_recv_cb(onData);
 }
