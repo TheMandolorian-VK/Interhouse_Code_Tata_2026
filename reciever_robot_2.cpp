12a3cd42bf1101e89210d1f870fa29de94c23bb2
TheMandolorian-VK <vivaan.446.2029@doonschool.com>
2026-04-19 20:47:39 +0530

    Fix onData function and WiFi include
    Updated function signature for onData and corrected Wifi to WiFi.

--- reciever_robot_2.cpp
+++ reciever_robot_2.cpp
@@ -1,15 +1,17 @@
 #include <esp_now.h>
-#include <Wifi.h>
+#include <WiFi.h>
 
-void onData(const uint_8 *data, const uint8_t *mac, int len){
+void onData(const esp_now_recv_info *info, const uint8_t *data, int len) {
   uint8_t cmd = data[0];
+  // 'info->src_addr' is now where the MAC address lives
 }
+
 void setup(){
   Wifi.mode(WIFI_STA);
   esp_now_init();
   esp_now_register_recv_cb(onData);
 }
 
-void loop{
+void loop(){
   
 }
