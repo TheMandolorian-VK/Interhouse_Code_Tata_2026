cb15c7f01fc0297f944fde4bf23ccc22451388d1
TheMandolorian-VK <vivaan.446.2029@doonschool.com>
2026-04-19 20:39:45 +0530

    Add sender_robot_1.cpp for ESP-NOW communication

---
+++ sender_robot_1.cpp
@@ -0,0 +1,22 @@
+// Include libraries
+#include <esp_now.h>
+#include <WiFi.h>
+
+// REPLACE WITH YOUR RECEIVER'S MAC ADDRESS
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+void setup() {
+  WiFi.mode(WIFI_STA); // Must be in station mode
+  esp_now_init();
+
+  esp_now_peer_info_t peer = {};
+  memcpy(peer.peer_addr, broadcastAddress, 6);
+  esp_now_add_peer(&peer);
+    
+}
+
+void loop() {
+  uint_8 code = 1; //placeholder value for now
+  esp_now_send(broadcastAddress, code, 1);
+  delay(2000); 
+}
