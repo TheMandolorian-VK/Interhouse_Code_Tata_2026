663492bb68d615f1bb1b17fdec7a62c74664595f
TheMandolorian-VK <vivaan.446.2029@doonschool.com>
2026-04-23 12:14:44 +0530

    Update and rename sender_robot_1.cpp to line_follower.cpp

--- line_follower.cpp
+++ line_follower.cpp
@@ -1,30 +1,32 @@
+#include <tuple>
+
 bool running = true;
 
 class Vehicle{
   private:
-    int _leftIR, _rightIR, _motorL1, _motorR2, _motorL2, _motorR1, _enA, _enB;
+    int _leftIR, _rightIR, _motorL1, _motorL2, _motorR1, _motorR2, _enA, _enB;
   public:
     Vehicle(int leftIR, int rightIR, int motorL1, int motorR1, int motorL2, int motorR2, int enA, int enB){
       _leftIR = leftIR;
       _rightIR = rightIR;
       _motorL1 = motorL1;
-      _motorR1 = motorR1;
       _motorL2 = motorL2;
+      _motorR1 = motorR1;
       _motorR2 = motorR2;
       _enA = enA;
       _enB = enB;
 
       pinMode(_leftIR, INPUT);
       pinMode(_rightIR, INPUT);
-      pinMode(_motorR1, OUTPUT);
       pinMode(_motorL1, OUTPUT);
-      pinMode(_motorR2, OUTPUT);
       pinMode(_motorL2, OUTPUT);
+      pinMode(_motorR1, OUTPUT);
+      pinMode(_motorR2, OUTPUT);
       pinMode(_enA, OUTPUT);
       pinMode(_enB, OUTPUT);
 
-      analogWrite(_enA, 200); 
-      analogWrite(_enB, 200);
+      analogWrite(_enA, 150); 
+      analogWrite(_enB, 150);
       
     }
 
@@ -44,18 +46,14 @@ class Vehicle{
     digitalWrite(_motorL1, LOW); digitalWrite(_motorL2, LOW);
     digitalWrite(_motorR1, LOW); digitalWrite(_motorR2, LOW);
   }
-  bool returnLSensorState(){
+  std::pair<int, int> returnSensorStates(){
     int sensorLeftState = digitalRead(_leftIR);
-    return sensorLeftState;
-  }
-  bool returnRSensorState(){
     int sensorRightState = digitalRead(_rightIR);
-    return sensorRightState;
+    return {sensorLeftState, sensorRightState};
   }
-  
 };
 
-Vehicle LineFollower(1, 2, 3, 4, 5, 6, 7, 8);
+Vehicle LineFollower(12, 13, 22, 19, 21, 18, 23, 5);
 
 void setup(){
   //enter in pin numbers tomorrow after checking connections
@@ -63,13 +61,12 @@ void setup(){
 
 void loop(){
   while(running){
-    bool sLstate = LineFollower.returnLSensorState();
-    bool sRstate = LineFollower.returnRSensorState();
+    auto [sLstate, sRstate] = LineFollower.returnSensorStates();
     if(sLState == LOW && sRstate == LOW){
       LineFollower.moveForward();
-    }else if(slState == LOW && sRstate == HIGH){
+    }else if(sLState == LOW && sRstate == HIGH){
       LineFollower.turnRight();
-    }else if(slState == HIGH && sRstate == LOW){
+    }else if(sLState == HIGH && sRstate == LOW){
       LineFollower.turnLeft();
     }
   
