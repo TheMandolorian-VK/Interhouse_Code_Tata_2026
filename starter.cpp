146db4085de82daf334315d6aa92407f5008a29c
TheMandolorian-VK <vivaan.446.2029@doonschool.com>
2026-04-27 15:40:06 +0530

    Update starter.cpp

--- starter.cpp
+++ starter.cpp
@@ -1,14 +1,21 @@
-int out1 = 22, out2 = 21, enA = 23;
+int out1 = 22, out2 = 21, enA = 23, out3 = 19, out4 = 18, enB = 15, IR = 13;
 
 void setup(){
   pinMode(out1, OUTPUT);
   pinMode(out2, OUTPUT);
+  pinMode(out3, OUTPUT);
+  pinMode(out4, OUTPUT);
   pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  analogWrite(enA, 200);
+  analogWrite(enB, 200);
   Serial.begin(9600);
 }
 
 void loop(){
-  analogWrite(enA, 200);
+  Serial.println("HIGH");
   digitalWrite(out1, HIGH);
   digitalWrite(out2, LOW);
+  digitalWrite(out3, HIGH);
+  digitalWrite(out4, LOW);
 }
